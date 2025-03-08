614d4aa6e21ce893291999576e2c0b09b6eabc43
kostasgr100 <97601678+kostasgr100@users.noreply.github.com>
2025-03-09 01:16:24 +0200

    Update recvmsg.rs

--- src/io/recvmsg.rs
+++ src/io/recvmsg.rs
@@ -28,7 +28,7 @@ impl<T: BoundedBufMut> Op<RecvMsg<T>> {
             }));
         }
 
-        let socket_addr = Box::new(unsafe { socket2::SockAddr::try_init(|_, _| Ok(()))? });
+        let socket_addr = Box::new(unsafe { SockAddr::try_init(|_, _| Ok(()))?.1 });
 
         let mut msghdr: Box<libc::msghdr> = Box::new(unsafe { std::mem::zeroed() });
         msghdr.msg_iov = io_slices.as_mut_ptr().cast();
